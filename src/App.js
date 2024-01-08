39af816eccaccfa9be02461095f09a88f219793c
Dmitry <dmitry27zh@gmail.com>
2024-01-08 17:45:14 +0500

    Add header template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 function App() {
-  return <div className="App">Hello, World!</div>
+  return (
+    <div className="wrapper">
+      <header className="header">
+        <a className="logo" href="/">
+          <svg />
+          <div className="logo__info">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </a>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <svg />
+            <span>1205 руб.</span>
+          </li>
+        </ul>
+      </header>
+      <main className="content">
+        <h1>Все кроссовки</h1>
+      </main>
+    </div>
+  )
 }
 
 export default App
