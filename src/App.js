b11539fe865254bacb8ae5eaa7b17c7b0093fee8
Dmitry <dmitry27zh@gmail.com>
2024-01-08 19:05:43 +0500

    Add header styles

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { ReactComponent as ProfileIcon } from '../src/assets/icons/profile.svg'
 
 function App() {
   return (
-    <div className="wrapper">
-      <header className="header">
-        <a className="logo" href="/">
-          <img width={40} height={40} src={logo} alt="Logo" />
+    <div className="wrapper clear">
+      <header className="header d-flex justify-between align-center p-40">
+        <a className="logo d-flex align-center" href="/">
+          <img className="logo__icon" width={40} height={40} src={logo} alt="Logo" />
           <div className="logo__info">
-            <h3>React Sneakers</h3>
+            <h3 className="text-uppercase">React Sneakers</h3>
             <p>Магазин лучших кроссовок</p>
           </div>
         </a>
-        <ul className="navigation">
+        <ul className="navigation d-flex flex-wrap">
           <li className="navigation__item">
             <CartIcon />
             <span>1205 руб.</span>
@@ -29,7 +29,7 @@ function App() {
           </li>
         </ul>
       </header>
-      <main className="content">
+      <main className="content p-40">
         <h1>Все кроссовки</h1>
       </main>
     </div>
