05f9b43b6afc8b4b4d6ba9ced3b77b390f2edd17
Dmitry <dmitry27zh@gmail.com>
2024-01-09 19:58:16 +0500

    Add cart

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { ReactComponent as TickIcon } from '../src/assets/icons/tick.svg'
 import { ReactComponent as HeartActiveIcon } from '../src/assets/icons/heart-active.svg'
 import { ReactComponent as HeartInactiveIcon } from '../src/assets/icons/heart-inactive.svg'
 import { ReactComponent as SearchIcon } from '../src/assets/icons/search.svg'
+import { ReactComponent as DeleteIcon } from '../src/assets/icons/delete.svg'
+import { ReactComponent as ArrowRightIcon } from '../src/assets/icons/arrow-right.svg'
 import sneakers1 from './assets/imgs/sneakers-1.jpg'
 import sneakers2 from './assets/imgs/sneakers-2.jpg'
 import sneakers3 from './assets/imgs/sneakers-3.jpg'
@@ -21,6 +23,56 @@ import sneakers10 from './assets/imgs/sneakers-10.jpg'
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="drawer">
+        <div className="drawer__overlay"></div>
+        <div className="drawer__content d-flex flex-column">
+          <div className="drawer__title d-flex justify-between align-center mb-30">
+            <h2>Корзина</h2>
+            <button className="cart-item__btn button card__favorite-btn" aria-label="Remove">
+              <DeleteIcon className="button__icon" width={32} height={32} aria-hidden={true} />
+            </button>
+          </div>
+
+          <div className="cart d-flex flex-column">
+            <article className="cart-item d-flex align-center mb-20">
+              <img className="cart-item__img mr-20" width={70} height={70} src={sneakers1} alt="Sneakers" />
+              <div className="cart-item__info mr-15">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="cart-item__btn button card__favorite-btn" aria-label="Remove">
+                <DeleteIcon className="button__icon" width={32} height={32} aria-hidden={true} />
+              </button>
+            </article>
+            <article className="cart-item d-flex align-center mb-20">
+              <img className="cart-item__img mr-20" width={70} height={70} src={sneakers2} alt="Sneakers" />
+              <div className="cart-item__info mr-15">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>8 499 руб.</b>
+              </div>
+              <button className="cart-item__btn button card__favorite-btn" aria-label="Remove">
+                <DeleteIcon className="button__icon" width={32} height={32} aria-hidden={true} />
+              </button>
+            </article>
+            <footer className="cart__footer d-flex flex-column">
+              <ul className="cart__info d-flex flex-column mb-25">
+                <li className="cart__info-item d-flex">
+                  <span>Итого: </span>
+                  <b>21 498 руб. </b>
+                </li>
+                <li className="cart__info-item d-flex">
+                  <span>Налог 5%: </span>
+                  <b>1074 руб. </b>
+                </li>
+              </ul>
+              <button className="button-color" type="button">
+                <span>Оформить заказ</span>
+                <ArrowRightIcon className="button-color__icon" width={16} height={14} />
+              </button>
+            </footer>
+          </div>
+        </div>
+      </div>
       <header className="header d-flex justify-between align-center p-40">
         <a className="logo d-flex align-center" href="/">
           <img className="logo__icon" width={40} height={40} src={logo} alt="Logo" />
