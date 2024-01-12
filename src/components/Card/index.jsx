4c4256d20845b8ddca07d45fe49f3c7282f6d27f
Dmitry <dmitry27zh@gmail.com>
2024-01-12 19:36:09 +0500

    Extract Card module styles

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,6 +2,7 @@ import { ReactComponent as PlusIcon } from '../../assets/icons/plus.svg'
 import { ReactComponent as TickIcon } from '../../assets/icons/tick.svg'
 import { ReactComponent as HeartActiveIcon } from '../../assets/icons/heart-active.svg'
 import { ReactComponent as HeartInactiveIcon } from '../../assets/icons/heart-inactive.svg'
+import styles from './Card.module.scss'
 
 const currency = new Intl.NumberFormat('ru-RU', {
   style: 'currency',
@@ -10,20 +11,20 @@ const currency = new Intl.NumberFormat('ru-RU', {
 
 const Card = ({ title, price, imgSrc, isFavorite, isAdded }) => {
   return (
-    <article className="card p-20">
+    <article className={`${styles.card} p-20`}>
       {isFavorite ? (
-        <button className="button card__favorite-btn" aria-label="Not favorite">
+        <button className={`button ${styles['card__favorite-btn']}`} aria-label="Not favorite">
           <HeartActiveIcon className="button__icon" width={32} height={32} aria-hidden={true} />
         </button>
       ) : (
-        <button className="button card__favorite-btn" aria-label="To favorite">
+        <button className={`button ${styles['card__favorite-btn']}`} aria-label="To favorite">
           <HeartInactiveIcon className="button__icon" width={32} height={32} aria-hidden={true} />
         </button>
       )}
       <img width={113} height={112} src={imgSrc} alt="Product" />
-      <h3 className="card__title">{title}</h3>
+      <h3 className={styles['card__title']}>{title}</h3>
       <footer className="d-flex justify-between align-center">
-        <div className="card__price d-flex flex-column">
+        <div className={`${styles['card__price']} d-flex flex-column`}>
           <span>Цена:</span>
           <b>{currency.format(price)}</b>
         </div>
