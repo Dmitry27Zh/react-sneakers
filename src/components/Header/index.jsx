b6220e580dbac57a2b3d862c7ffb7999fdce208a
Dmitry <dmitry27zh@gmail.com>
2024-01-17 20:01:33 +0500

    Add home Link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,17 @@
+import { Link } from 'react-router-dom'
 import logo from '../../assets/icons/logo.png'
 import Navigation from '../Navigation'
 
 const Header = ({ onCartOpen }) => {
   return (
     <header className="header d-flex justify-between align-center p-40">
-      <a className="logo d-flex align-center" href="/">
+      <Link className="logo d-flex align-center" to="/">
         <img className="logo__icon" width={40} height={40} src={logo} alt="Logo" />
         <div className="logo__info">
           <h3 className="text-uppercase">React Sneakers</h3>
           <p className="opacity-5">Магазин лучших кроссовок</p>
         </div>
-      </a>
+      </Link>
       <Navigation onCartOpen={onCartOpen} />
     </header>
   )
