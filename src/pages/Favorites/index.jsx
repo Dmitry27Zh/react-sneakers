3f5e862e8edaa695970eabd6162978f0a87863f5
Dmitry <dmitry27zh@gmail.com>
2024-01-17 20:27:51 +0500

    Start Favorites

---
+++ src/pages/Favorites/index.jsx
@@ -0,0 +1,12 @@
+const Favorites = () => {
+  return (
+    <>
+      <div className="d-flex justify-between align-center mb-40">
+        <h1>Мои покупки</h1>
+      </div>
+      <div className="products d-flex flex-wrap">Favorites</div>
+    </>
+  )
+}
+
+export default Favorites
