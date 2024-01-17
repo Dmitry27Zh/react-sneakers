c35f026c7c7947f9fa59e46106c526fce1b8ff79
Dmitry <dmitry27zh@gmail.com>
2024-01-17 16:46:37 +0500

    Extract Cart module styles

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -2,12 +2,13 @@ import { ReactComponent as ArrowRightIcon } from '../../assets/icons/arrow-right
 import { ReactComponent as ArrowLeftIcon } from '../../assets/icons/arrow-left.svg'
 import CartItems from './CartItems'
 import boxEmptySrc from '../../assets/imgs/box-empty.png'
+import styles from './Cart.module.scss'
 
 const Cart = ({ items, onItemRemove }) => {
   return (
-    <div className="cart d-flex flex-column">
+    <div className={`${styles.cart} d-flex flex-column`}>
       {items.length === 0 ? (
-        <div className="cart__empty d-flex flex-column justify-center align-center">
+        <div className={`${styles['cart__empty']} d-flex flex-column justify-center align-center`}>
           <img src={boxEmptySrc} alt="Empty box" />
           <h2>Корзина пустая</h2>
           <p className="opacity-6">Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
@@ -19,13 +20,13 @@ const Cart = ({ items, onItemRemove }) => {
       ) : (
         <>
           <CartItems items={items} onItemRemove={onItemRemove} />
-          <footer className="cart__footer d-flex flex-column">
-            <ul className="cart__info d-flex flex-column mb-25">
-              <li className="cart__info-item d-flex">
+          <footer className={`${styles['cart__footer']} d-flex flex-column`}>
+            <ul className={`${styles['cart__info']} d-flex flex-column mb-25`}>
+              <li className={`${styles['cart__info-item']} d-flex`}>
                 <span>Итого: </span>
                 <b>21 498 руб. </b>
               </li>
-              <li className="cart__info-item d-flex">
+              <li className={`${styles['cart__info-item']} d-flex`}>
                 <span>Налог 5%: </span>
                 <b>1074 руб. </b>
               </li>
