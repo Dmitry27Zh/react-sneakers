a5a33dd8f4f36f13965ff65c6fb38192cae1096d
Dmitry <dmitry27zh@gmail.com>
2024-01-18 18:12:51 +0500

    Add skeleton in Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,3 +1,4 @@
+import ContentLoader from 'react-content-loader'
 import { ReactComponent as PlusIcon } from '../../assets/icons/plus.svg'
 import { ReactComponent as TickIcon } from '../../assets/icons/tick.svg'
 import { ReactComponent as HeartActiveIcon } from '../../assets/icons/heart-active.svg'
@@ -19,39 +20,65 @@ const Card = ({
   onCartRemove,
   onFavoriteAdd,
   onFavoriteRemove,
+  isLoading = true,
 }) => {
   return (
     <article className={`${styles.card} p-20`}>
-      {isFavorite ? (
-        <button
-          className={`button ${styles['card__favorite-btn']}`}
-          aria-label="Not favorite"
-          onClick={onFavoriteRemove}
+      {isLoading ? (
+        <ContentLoader
+          className="w100p"
+          speed={2}
+          width={150}
+          height={200}
+          viewBox="0 0 150 200"
+          backgroundColor="#f3f3f3"
+          foregroundColor="#ecebeb"
         >
-          <HeartActiveIcon className="button__icon" width={32} height={32} aria-hidden={true} />
-        </button>
+          <rect x="64" y="58" rx="0" ry="0" width="0" height="1" />
+          <rect x="0" y="0" rx="10" ry="10" width="150" height="90" />
+          <rect x="0" y="106" rx="3" ry="3" width="150" height="15" />
+          <rect x="0" y="135" rx="3" ry="3" width="90" height="15" />
+          <rect x="0" y="170" rx="8" ry="8" width="80" height="25" />
+          <rect x="118" y="165" rx="8" ry="8" width="32" height="32" />
+        </ContentLoader>
       ) : (
-        <button className={`button ${styles['card__favorite-btn']}`} aria-label="To favorite" onClick={onFavoriteAdd}>
-          <HeartInactiveIcon className="button__icon" width={32} height={32} aria-hidden={true} />
-        </button>
+        <>
+          {isFavorite ? (
+            <button
+              className={`button ${styles['card__favorite-btn']}`}
+              aria-label="Not favorite"
+              onClick={onFavoriteRemove}
+            >
+              <HeartActiveIcon className="button__icon" width={32} height={32} aria-hidden={true} />
+            </button>
+          ) : (
+            <button
+              className={`button ${styles['card__favorite-btn']}`}
+              aria-label="To favorite"
+              onClick={onFavoriteAdd}
+            >
+              <HeartInactiveIcon className="button__icon" width={32} height={32} aria-hidden={true} />
+            </button>
+          )}
+          <img width={113} height={112} src={imgSrc} alt="Product" />
+          <h3 className={styles['card__title']}>{title}</h3>
+          <footer className={`${styles['card__footer']} d-flex justify-between align-center`}>
+            <div className={`${styles['card__price']} d-flex flex-column`}>
+              <span>Цена:</span>
+              <b>{currency.format(price)}</b>
+            </div>
+            {isAdded ? (
+              <button className="button" aria-label="Remove" onClick={onCartRemove}>
+                <TickIcon className="button__icon" width={32} height={32} aria-hidden={true} />
+              </button>
+            ) : (
+              <button className="button" aria-label="Add" onClick={onCartAdd}>
+                <PlusIcon className="button__icon" width={32} height={32} aria-hidden={true} />
+              </button>
+            )}
+          </footer>
+        </>
       )}
-      <img width={113} height={112} src={imgSrc} alt="Product" />
-      <h3 className={styles['card__title']}>{title}</h3>
-      <footer className="d-flex justify-between align-center">
-        <div className={`${styles['card__price']} d-flex flex-column`}>
-          <span>Цена:</span>
-          <b>{currency.format(price)}</b>
-        </div>
-        {isAdded ? (
-          <button className="button" aria-label="Remove" onClick={onCartRemove}>
-            <TickIcon className="button__icon" width={32} height={32} aria-hidden={true} />
-          </button>
-        ) : (
-          <button className="button" aria-label="Add" onClick={onCartAdd}>
-            <PlusIcon className="button__icon" width={32} height={32} aria-hidden={true} />
-          </button>
-        )}
-      </footer>
     </article>
   )
 }
