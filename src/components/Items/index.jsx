059d733a243f902a6a66b5d220064edf0d9ad816
Dmitry <dmitry27zh@gmail.com>
2024-01-22 09:50:31 +0500

    Add isInteractive prop in Items

--- src/components/Items/index.jsx
+++ src/components/Items/index.jsx
@@ -9,26 +9,23 @@ const Items = ({
   handleFavoriteAdd,
   handleFavoriteRemove,
   isLoading,
+  isInteractive = true,
 }) => {
   const itemsToRender = isLoading ? Array.from({ length: 10 }, (_, index) => ({ _id: index })) : items
 
   return itemsToRender.map((item) => {
-    const isCart = cartItems.some((cartItem) => cartItem._id === item._id)
-    const isFavorite = favoriteItems.some((favoriteItem) => favoriteItem._id === item._id)
+    const props = isInteractive
+      ? {
+          isFavorite: favoriteItems.some((favoriteItem) => favoriteItem._id === item._id),
+          isAdded: cartItems.some((cartItem) => cartItem._id === item._id),
+          onCartAdd: () => handleCartAdd(item),
+          onCartRemove: () => handleCartRemove(item),
+          onFavoriteAdd: () => handleFavoriteAdd(item),
+          onFavoriteRemove: () => handleFavoriteRemove(item),
+        }
+      : {}
 
-    return (
-      <Card
-        key={item._id}
-        {...item}
-        isFavorite={isFavorite}
-        isAdded={isCart}
-        onCartAdd={() => handleCartAdd(item)}
-        onCartRemove={() => handleCartRemove(item)}
-        onFavoriteAdd={() => handleFavoriteAdd(item)}
-        onFavoriteRemove={() => handleFavoriteRemove(item)}
-        isLoading={isLoading}
-      />
-    )
+    return <Card key={item._id} isLoading={isLoading} isInteractive={isInteractive} {...item} {...props} />
   })
 }
 
